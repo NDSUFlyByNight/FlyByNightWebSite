1309d86fa650ee9a2af82a38654374e870dbcda0
Nathan Martins <nathanmrtns@gmail.com>
2015-12-14 14:57:16 -0600

    flight status added

--- flightStatus.php
+++ flightStatus.php
@@ -9,6 +9,9 @@ else{
     include('logged_header.php');
 }
 ?>
+
+To find one on time: look up FBN053
+<br>To find one delayed: look up FBN314
 <style type="text/css">
 #startDate, #endDate {
       background-color: white;
@@ -22,18 +25,63 @@ else{
         <br />
 		<div class="input-group input-group-sm">
             <div id="searchFlight">
-                <div class="col-xs-3">
-                    <span>Flight Date: <input class="form-control" type="text" name="flightDate" id="flightDate" required><span>
-                </div>
-                <div class="col-xs-3">
-                    <span>Flight Number: <input class="form-control" type="text" name="from" id="from" required><span>
-                </div>
+				<div class ="col=xs=3">
+					<span>Flight Number: <input class="form=control" type="text" name ="from" id="from" required></span>
+</div>
                 <div class="col-xs-2">
-                    <br>
                     <button type="submit" name="checkBtn" value"search" class="btn btn-success">View Status</button>
                 </div>
             <div>
 		</div>
 	</form>
 
-</html>
+
+<?php
+function findFlights ($flight)
+{
+	//Connects to database
+	require('connect_db.php');
+
+	$query = mssql_query('SELECT * FROM FLIGHT');
+
+	if (!mssql_num_rows($query)) {
+    	echo 'No records found';
+	}
+else
+{
+//Creates tables and fills it with flight numbers and their delays
+	echo '<br><br><br><br><table border = 1>';
+	echo '<th>Flight Number</th><th>Delayed</th><th>Depature Time</th>';
+	while ($row = mssql_fetch_assoc($query)){
+	$i =0;
+//Check if flight is what is looking for
+
+if (strcmp ($row['Flight_number'],$flight) == 0)
+{
+		$i = $i + 1;
+		echo '<tr><td>'. $row['Flight_number']. '</td>';
+if(strcmp($row['Delayed'], '1') != 0)
+{
+		echo '<td>'. 'On Time'. '</td>';
+}
+else
+{
+		echo '<td>'. 'Delayed'. '</td>';
+}
+echo '<td>'. $row['Depature_time'].'</td></tr>';
+//^End else
+}
+}
+//^ends while
+echo '</table>';
+}
+}
+//Gets info from user on flight wantings to look up
+
+if (isset($_POST['from'])) {
+		$date = print_r ($_POST['from'],true);
+    FindFlights($date);
+
+}
+
+?>
